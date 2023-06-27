b2f26e758877e7b6a7b063e85b50d0ee5b39840f
Atsushi-Noguchi <gunogunoatushi@gmail.com>
2023-06-27 12:35:09 +0900

    HTMLの共通化

--- src/pages/create.php
+++ src/pages/create.php
@@ -30,11 +30,35 @@ function validate($reviews)
 {
     $errors = [];
 
-    //タイトルのエラー処理
+    //タイトルのバリデーション処理
     if (!strlen($reviews['title'])) {
         $errors['title'] = '書籍名を入力してください。';
     } elseif (strlen($reviews['title'] > 255)) {
         $errors['title'] = '書籍名は２５５文字以内で入力してください。';
+    }
+
+    //著者名のバリデーション処理
+    if (!strlen($reviews['author'])) {
+        $errors['author'] = '著者名を入力してください。';
+    } elseif (strlen($reviews['author'] > 100)) {
+        $errors['author'] = '著者名は１００文字以内で入力してください。';
+    }
+
+    //読書状況のバリデーション処理
+    if (!in_array($reviews['status'], ['未読', '読書中', '読了'], true)) {
+        $errors['status'] = '読書状況は「未読」「読んでる」「読了」のいずれかを入力してください';
+    }
+
+    //評価の入力値のバリデーション処理
+    if ($reviews['score'] < 1 || $reviews['score'] > 5) {
+        $errors['score'] = '入力数値は1〜5の整数でお願いします。';
+    }
+
+    //感想のバリーデーション処理
+    if (!strlen($reviews['summary'])) {
+        $errors['summary'] = '感想を入力してください。';
+    } elseif (strlen($reviews['summary']) > 1000) {
+        $errors['summary'] = '感想は１０００文字以内で入力してください。';
     }
 
     return $errors;
@@ -42,11 +66,11 @@ function validate($reviews)
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $reviews = [
-        'title' => $_POST['title'],
-        'author' => $_POST['author'],
-        'status' => $_POST['status'],
-        'score' => $_POST['score'],
-        'summary' => $_POST['summary'],
+        'title' => isset($_POST['title']) ? $_POST['title'] : '',
+        'author' => isset($_POST['author']) ? $_POST['author'] : '',
+        'status' => isset($_POST['status']) ? $_POST['status'] : '',
+        'score' => isset($_POST['score']) ? $_POST['score'] : '',
+        'summary' => isset($_POST['summary']) ? $_POST['summary'] : '',
     ];
 
     $errors = validate($reviews);
@@ -58,64 +82,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header("Location: index.php");
     }
 }
-?>
 
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>読書ログ登録</title>
-</head>
-
-<body>
-    <h1>読書ログ</h1>
-    <form action="create.php" method="post">
-        <?php
-        if (count($errors)) : ?>
-            <ul>
-                <?php foreach ($errors as $error) : ?>
-                    <li><?php echo $error; ?></li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-
-        <div>
-            <label for="title">書籍名</label>
-            <input type="text" name="title" id="title">
-        </div>
-        <div>
-            <label for="author">著者名</label>
-            <input type="text" name="author" id="author">
-        </div>
-        <div>
-            <label>読書状況</label>
-            <div>
-                <div>
-                    <input type="radio" name="status" id="status1" value="未読">
-                    <label for="status1">未読</label>
-                </div>
-                <div>
-                    <input type="radio" name="status" id="status2" value="読んでる">
-                    <label for="status2">読んでる</label>
-                </div>
-                <div>
-                    <input class="form-check-input" type="radio" name="status" id="status3" value="読了">
-                    <label for="status3">読了</label>
-                </div>
-            </div>
-        </div>
-        <div>
-            <label for="score">評価（5点満点の整数）</label>
-            <input type="number" name="score" id="score">
-        </div>
-        <div>
-            <label for="summary">感想</label>
-            <textarea type="text" name="summary" id="summary" rows="10"></textarea>
-        </div>
-        <button type="submit">登録する</button>
-    </form>
-</body>
-
-</html>
+include 'register.php';
